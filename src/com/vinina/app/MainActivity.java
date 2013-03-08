c87740ac05569c58afbe7a6c9789eaf75ddbad30
phucdat1105 <quangdoan.store@gmail.com>
2013-03-08 19:18:50 +0700

    CHANGE
    Change interface

--- src/com/vinina/app/MainActivity.java
+++ src/com/vinina/app/MainActivity.java
@@ -3,14 +3,35 @@ package com.vinina.app;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity  implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       
+        Button b = (Button)this.findViewById(R.id.btn_confirm);
+		b.setOnClickListener(this);
+
+       
     }
+    @Override
+	public void onClick(View v) {
+		TextView tv = (TextView)this.findViewById(R.id.tv_welcome);
+		EditText et = (EditText)this.findViewById(R.id.txt_name);
+
+		String text = "Hello, " + et.getText().toString() + ".\n\n";
+		text += "Welcome to android development. :)";
+		
+		tv.setText(text);
+	}
 
 
     @Override
